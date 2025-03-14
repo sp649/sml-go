1af4946cebfee52757d8dbda9dd1d5c53ab649b2
SP649 <158019744+sp649@users.noreply.github.com>
2025-03-13 23:52:36 -0400

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sml-parser
+
+go 1.24.1
---
+++ parser.go
@@ -0,0 +1,55 @@
+package parser
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+)
+
+func ParseForAsArray(data, dname string) string[] {
+	
+	var words []string
+	
+	scanner := bufio.NewScanner(data)
+	
+	for scanner.Scan() {
+		
+		line := scanner.Text()
+		words := strings.Fields(line)
+
+		ifword := strings.TrimSuffix(words[0], ":")
+		
+		if ifword == dname {
+			return words[1:]
+                } else {
+			return "Couldnt not be found! :C"
+		}
+
+        }
+
+}
+
+func ParseFordata, dname string) string {
+	
+	var words []string
+	
+	scanner := bufio.NewScanner(data)
+	
+	for scanner.Scan() {
+		
+		line := scanner.Text()
+		words := strings.Fields(line)
+
+		ifword := strings.TrimSuffix(words[0], ":")
+		
+		if ifword == dname {
+			return strings.Join(words[1:], " ")
+                } else {
+			return "Couldnt not be found! :C"
+		}
+
+        }
+
+}
+
